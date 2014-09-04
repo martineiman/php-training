e12e55c5d85f71100fb824cad4d529581a730e4e
Erwin <fedeerico_b@gmail.com git config --global user.name Erwin git commit -m Primer>
2014-09-04 20:52:57 -0300

    Acceder a base de datos con funcion

--- Basedatos/AccederBD.php
+++ Basedatos/AccederBD.php
@@ -5,7 +5,8 @@
  
  */
 
-$mySqli = mysqli_connect("localhost", "martin", "1q2w3e4r", "listatareas");
+$usu= 'Lucho Gaba';
+/*$mySqli = mysqli_connect("localhost", "martin", "1q2w3e4r", "listatareas");
 $res = mysqli_query($mySqli,"SELECT * from usuarios" );
 $row = mysqli_fetch_assoc($res);
 
@@ -14,6 +15,24 @@ while ($row!=NULL) {
     $row = mysqli_fetch_assoc($res);
     echo "<br>";
 }
-
+*/
+function tareasXusu($usu){
+    $mySqli = mysqli_connect("localhost", "martin", "1q2w3e4r", "listatareas");
+    $res = mysqli_query($mySqli,"SELECT usuId FROM `usuarios` WHERE `usuNyA`= '$usu'" );
+     $user = mysqli_fetch_assoc($res);
+    $res2= mysqli_query($mySqli,"SELECT tareaNombre FROM `tareas` WHERE `usuId`=".$user['usuId'] );
+    
+    $row = mysqli_fetch_assoc($res2);
+    
+    while ($row!=NULL) {
+    echo "<li>".$row['tareaNombre']."</li>";
+    $row = mysqli_fetch_assoc($res);
+    echo "<br>";
+    
+    }
+ 
+}
+tareasXusu($usu);
     
 ?>
+
